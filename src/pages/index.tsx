161d4de33aad2c0f150c9b150fdf6033e847faba
JophielZX <396038902@qq.com>
2020-03-20 11:55:34 +0800

    从 umi 引入 useLocation 并使用在组件中

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import { useLocation } from 'umi';
 import styles from './index.less';
 
 export default () => {
+  const { pathname } = useLocation();
+
   return (
     <div>
       <h1 className={styles.title}>Page index</h1>
+      <div>pathname: {pathname}</div>
     </div>
   );
-}
+};
